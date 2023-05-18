7eaf982541053d6ef3de2968d8e5fac8c9201f0d
Komal <komal.sahay@tigeranalytics.com>
2023-05-18 17:51:10 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # create a virtual environment with python 3.8
-python3.8 -m venv annotator-webapp-env
+conda create --name webapp python=3.8.16
 
 # activate the virtual environment
-source annotator-webapp-env/bin/activate
+source activate webapp
 
 # install all packages from requirements.txt
 pip install -r requirements.txt
